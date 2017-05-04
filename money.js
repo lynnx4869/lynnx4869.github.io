c5c3efbb00d1a7cb5c46748e309d1e5c3508b3b5
夏宁 <lynnx4869@icloud.com>
2017-05-04 20:19:24 +0800

    add snow

--- money.js
+++ money.js
@@ -1,5 +1,30 @@
 (() => {
 
+    function snow(left, height, src) {
+        let div = document.createElement('div');
+        let img = document.createElement('img');
 
+        $(div).css({
+                left: left + 'px',
+                height: height + 'px'
+            })
+            .addClass('fix');
+        $(img).addClass('roll')
+            .attr('src', src);
+
+        $(div).append(img);
+        $('.container').append(div);
+
+        setTimeout(() => {
+            $(div).remove();
+        }, 5000);
+    }
+
+    setInterval(() => {
+        let left = Math.random() * window.innerWidth;
+        let height = Math.random() * window.innerHeight;
+        let src = './apple.png';
+        snow(left, height, src);
+    }, 500);
 
 })();
