59f54eb5473e8cc1e172b7177b6f0e54dc54d7ae
夏宁 <n.xia@ctrip.com>
2019-08-07 16:43:21 +0800

    change config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+// const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    mode: 'development', //'production',
-    entry: './App/App.tsx',
-    output: {
-        filename: 'bundle.js'
+    entry: {
+        main: './App/App.tsx'
     },
+
+    output: {
+        filename: '[name].bundle.js'
+    },
+
     module: {
         rules: [
             {
@@ -18,9 +25,37 @@ module.exports = {
             }
         ]
     },
+
     resolve: {
         modules: ['node_modules'],
-        extensions: ['.js', '.json', '.jsx', '.ts', '.tsx', '.css', '.html'],
+        extensions: ['.js', '.json', '.jsx', '.ts', '.tsx', '.css', '.html', '.png', '.jpg'],
     },
+
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            name: true,
+            cacheGroups: {
+                commons: {
+                    name: 'commons',
+                    filename: 'commons.js',
+                    test: /[\\/]node_modules[\\/]/
+                }
+            }
+        }
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'main.html',
+            template: './UI/index.html',
+            hash: true,
+            chunks: ['main', 'commons']
+        })
+    ],
+
+    mode: 'production', // production development
+
     devtool: 'inline-source-map'
 };
